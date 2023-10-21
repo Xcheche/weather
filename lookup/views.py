1dbc727899692553778e929233b148dc525e25cf
xcheche <chekwubechukwu23@gmail.com>
2023-10-21 14:25:43 +0100

    jumbotron added

--- lookup/views.py
+++ lookup/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+import json
+import requests
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'home.html',{})
+    try:
+        api_request = requests.get(
+            "https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=FD6CD5F5-21E5-4E9F-BC67-6A2F7FE03C36")
+
+        # Check the status code to ensure the request was successful
+        if api_request.status_code == 200:
+            api = json.loads(api_request.content)
+        else:
+            api = {"error": "API request failed with status code {}".format(
+                api_request.status_code)}
+    except Exception as e:
+        api = {"error": str(e)}
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
-    return render(request, 'about.html',{})
+    return render(request, 'about.html', {})
